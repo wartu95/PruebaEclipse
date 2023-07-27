0215aeeda2a408485535a4af31c140de0de1e2c5
wartu95 <113658802+wartu95@users.noreply.github.com>
2023-07-27 15:33:45 -0500

    Comienzo de proyecto

---
+++ src/aaa/ejemploGit.java
@@ -0,0 +1,11 @@
+package aaa;
+
+public class ejemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola alumnos");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author elwar
+ *
+ */
+module PruebaGit {
+}
